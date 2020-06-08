1279dd86fb5e34215f335851985c9c50246f4e64
christian <ChristiaNotarmaso@gmail.com>
2020-06-08 17:40:57 +0200

    DAO

--- src/main/DAL/Classes/ProduktBatchDAO.java
+++ src/main/DAL/Classes/ProduktBatchDAO.java
@@ -1,9 +1,12 @@
 package DAL.Classes;
 
 import DAL.DALException;
+import DAL.DBTablePrinter;
 import DAL.Interfaces.IProduktBatchDAO;
 import DTO.ProduktBatchDTO;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProduktBatchDAO implements IProduktBatchDAO {
@@ -20,20 +23,131 @@ public class ProduktBatchDAO implements IProduktBatchDAO {
     private String url = "jdbc:mysql://" + this.host + ":" + this.port + this.database + "?characterEncoding=latin1&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
 
+    public static void main(String[] args) {
+        ProduktBatchDAO test = new ProduktBatchDAO();
+
+        try {
+            test.getProduktBatchList();
+        } catch (DALException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+
     @Override
     public ProduktBatchDTO getProduktBatch(int pbId) throws DALException {
-        return null;
+
+        try {
+            Class.forName(this.driver);
+
+            String sqlManipulation;
+
+            sqlManipulation = "SELECT * FROM ProduktBatch WHERE pbID='" + pbId + "'";
+
+            Connection connection = DriverManager.getConnection(this.url, this.username, this.password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlManipulation);
+
+            if(resultSet.next()){
+
+                ProduktBatchDTO batch = new ProduktBatchDTO();
+                batch.setPbId(resultSet.getInt("pbId"));
+                batch.setStatus(resultSet.getInt("status"));
+                batch.setReceptId(resultSet.getInt("receptId"));
+                connection.close();
+
+                System.out.println("pbID:" + batch.getPbId() + " Status:" + batch.getStatus() + " ReceptId:" + batch.getReceptId());
+                return batch;
+
+
+            }else {
+                connection.close();
+                throw new DALException("Kunne ikke finde produkt batch");
+            }
+
+        } catch (SQLException | ClassNotFoundException e ) {
+            e.printStackTrace();
+            throw new DALException("Database fejl");
+
+        }
+
     }
 
     @Override
     public List<ProduktBatchDTO> getProduktBatchList() throws DALException {
-        return null;
+
+            List<ProduktBatchDTO> produktListe = new ArrayList<>();
+
+        try {
+            Class.forName(this.driver);
+
+            String sqlManipulation;
+
+            sqlManipulation = "SELECT * FROM ProduktBatch ";
+
+            Connection connection = DriverManager.getConnection(this.url, this.username, this.password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlManipulation);
+
+
+            while (resultSet.next()){
+                ProduktBatchDTO batch = new ProduktBatchDTO();
+                batch.setPbId(resultSet.getInt("pbId"));
+                batch.setStatus(resultSet.getInt("status"));
+                batch.setReceptId(resultSet.getInt("receptId"));
+
+                produktListe.add(batch);
+            }
+            connection.close();
+
+
+            String out = "pbId | status | receptId";
+            for(int i = 0; i<produktListe.size(); i++){
+
+                out += "\n" + produktListe.get(i).getPbId() +"\t\t" + produktListe.get(i).getStatus() +"\t\t"+ produktListe.get(i).getReceptId();
+
+            }
+            System.out.println(out);
+
+
+            return produktListe;
+
+
+        } catch (SQLException | ClassNotFoundException e ) {
+            e.printStackTrace();
+            throw new DALException("Database fejl");
+
+        }
+
     }
+
+
 
     @Override
     public void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException {
 
+        try {
+            Class.forName(this.driver);
 
+            String sqlManipulation = "INSERT Userlist VALUES ('" + user.getUserId() + "', '" + user.getUserName() + "', '" + user.getIni() + "', '" + user.getCpr() + "', '" + user.getPassword() + "')";
+
+            Connection connection = DriverManager.getConnection(this.url, this.username, this.password);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sqlManipulation);
+
+            for (String role : user.getRoles()) {
+                sqlManipulation = "INSERT UserRoles VALUES( " + user.getUserId() + " , \"" + role + "\")";
+                statement.executeUpdate(sqlManipulation);
+            }
+            connection.close();
+
+
+        }  catch (SQLException | ClassNotFoundException e ) {
+            e.printStackTrace();
+            throw new DALException("Database fejl");
+
+        }
 
     }
 
