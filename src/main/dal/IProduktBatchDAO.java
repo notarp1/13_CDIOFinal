b4a40339d265144d50322809e33ed606fd170f10
christian <ChristiaNotarmaso@gmail.com>
2020-06-08 14:37:35 +0200

    Folders

--- src/main/dal/IProduktBatchDAO.java
+++ src/main/dal/IProduktBatchDAO.java
@@ -6,7 +6,9 @@ import java.util.List;
 
 public interface IProduktBatchDAO {
     ProduktBatchDTO getProduktBatch(int pbId) throws DALException;
+
     List<ProduktBatchDTO> getProduktBatchList() throws DALException;
+
     void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
     void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
 
