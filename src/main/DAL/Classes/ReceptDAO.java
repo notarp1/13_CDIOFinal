ce5d64327f00fa7af24027f47634ac76bd870e68
Ismail <s195460@student.dtu.dk>
2020-06-09 21:58:30 +0200

    DAO for recept/receptKomp

--- src/main/DAL/Classes/ReceptDAO.java
+++ src/main/DAL/Classes/ReceptDAO.java
@@ -3,27 +3,126 @@ package DAL.Classes;
 import DAL.DALException;
 import DAL.Interfaces.IReceptDAO;
 import DTO.ReceptDTO;
+import DTO.ReceptKompDTO;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReceptDAO implements IReceptDAO {
+    private String host = "primary.folkmann.it";
+    private String port = "3306";
+    private String username = "CDIO";
+    private String password = "y1NzaOYI08FrdqzX";
+    private String database = "/DTU_CDIOFinal";
+
+    //Do not edit these variables
+    private String driver = "com.mysql.cj.jdbc.Driver";
+    private String url = "jdbc:mysql://" + this.host + ":" + this.port + this.database + "?characterEncoding=latin1&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+
+    private ReceptDTO getRecept(ResultSet resultSet) throws SQLException {
+
+        ReceptDTO recept = new ReceptDTO();
+        recept.setReceptId(resultSet.getInt("receptId"));
+        recept.setReceptNavn(resultSet.getString("receptNavn"));
+        return recept;
+
+    }
+
+
     @Override
     public ReceptDTO getRecept(int receptId) throws DALException {
+        try {
+            Class.forName(this.driver);
+            Connection connection = DriverManager.getConnection(this.url, this.username, this.password);
+
+            String sqlManipulation = "SELECT * FROM Recept WHERE receptId=" + receptId;
+
+            Statement statement =  connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlManipulation);
+
+            resultSet.next();
+            ReceptDTO recept = getRecept(resultSet);
+            connection.close();
+
+            return recept;
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("findes ikke");
+        }
         return null;
     }
 
     @Override
     public List<ReceptDTO> getReceptList() throws DALException {
+
+        List<ReceptDTO> receptList = new ArrayList<>();
+
+        try {
+            Class.forName(this.driver);
+            Connection connection = DriverManager.getConnection(this.url, this.username, this.password);
+
+            String sqlManipulation = "SELECT * FROM Recept";
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlManipulation);
+
+
+           while (resultSet.next()) {
+               ReceptDTO recept = getRecept(resultSet);
+
+               receptList.add(recept);
+
+           }
+            connection.close();
+
+           return receptList;
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("findes ikke");
+        }
+
         return null;
     }
 
+
     @Override
     public void createRecept(ReceptDTO recept) throws DALException {
+        try {
+            Class.forName(this.driver);
+            String sqlManipulation = "INSERT Recept VALUES ('" + recept.getReceptId() +  "', '" + recept.getReceptNavn() + "')";
+
+            Connection connection = DriverManager.getConnection(this.url, this.username, this.password);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sqlManipulation);
+            connection.close();
+
+        } catch (SQLException | ClassNotFoundException e ) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void updateRecept(ReceptDTO recept) throws DALException {
+
+        try {
+            Class.forName(this.driver);
+            String sqlManipulation = "UPDATE Recept SET receptId = " + recept.getReceptId() + ", receptNavn = '" + recept.getReceptNavn() + "' WHERE receptId = " + recept.getReceptId();
+            Connection connection = DriverManager.getConnection(this.url, this.username, this.password);
+
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sqlManipulation);
+            connection.close();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
--- src/main/DAL/Classes/ReceptKompDAO.java
+++ src/main/DAL/Classes/ReceptKompDAO.java
@@ -2,33 +2,161 @@ package DAL.Classes;
 
 import DAL.DALException;
 import DAL.Interfaces.IReceptKompDAO;
+import DTO.ProduktBatchKompDTO;
 import DTO.ReceptKompDTO;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReceptKompDAO implements IReceptKompDAO {
+    private String host = "primary.folkmann.it";
+    private String port = "3306";
+    private String username = "CDIO";
+    private String password = "y1NzaOYI08FrdqzX";
+    private String database = "/DTU_CDIOFinal";
+
+    //Do not edit these variables
+    private String driver = "com.mysql.cj.jdbc.Driver";
+    private String url = "jdbc:mysql://" + this.host + ":" + this.port + this.database + "?characterEncoding=latin1&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+
+    private ReceptKompDTO getReceptKomp(ResultSet resultSet) throws SQLException {
+        ReceptKompDTO receptKomp =  new ReceptKompDTO();
+        receptKomp.setReceptId(resultSet.getInt("receptId"));
+        receptKomp.setRaavareId(resultSet.getInt("raavareId"));
+        receptKomp.setNomNetto(resultSet.getDouble("nomNetto"));
+        receptKomp.setTolerance(resultSet.getDouble("tolerance"));
+        return receptKomp;
+    }
+
     @Override
     public ReceptKompDTO getReceptKomp(int receptId, int raavareId) throws DALException {
+         try{
+            Class.forName(this.driver);
+            Connection connection = DriverManager.getConnection(this.url, this.username, this.password);
+
+            String sqlManipulation = "SELECT * FROM ReceptKomp WHERE receptId='" + receptId + "' AND raavareId ='" + raavareId + "'";
+
+            Statement statement =  connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlManipulation);
+
+            resultSet.next();
+            ReceptKompDTO receptKomp =  getReceptKomp(resultSet);
+            connection.close();
+
+            return receptKomp;
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("findes ikke");
+        }
         return null;
     }
 
     @Override
     public List<ReceptKompDTO> getReceptKompList(int receptId) throws DALException {
+
+        List<ReceptKompDTO> receptKompList = new ArrayList<>();
+
+        try {
+            Class.forName(this.driver);
+            Connection connection = DriverManager.getConnection(this.url, this.username, this.password);
+
+            String sqlManipulation = "SELECT * FROM ReceptKomp where receptId='" + receptId + "'";
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlManipulation);
+
+            while (resultSet.next()) {
+                ReceptKompDTO receptKomp = getReceptKomp(resultSet);
+
+                receptKompList.add(receptKomp);
+            }
+            connection.close();
+
+            return receptKompList;
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("findes ikke");
+        }
+
+
+
         return null;
     }
+
 
     @Override
     public List<ReceptKompDTO> getReceptKompList() throws DALException {
+
+        List<ReceptKompDTO> receptKompList = new ArrayList<>();
+
+        try {
+            Class.forName(this.driver);
+            Connection connection = DriverManager.getConnection(this.url, this.username, this.password);
+
+            String sqlManipulation = "SELECT * FROM ReceptKomp";
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sqlManipulation);
+
+            String DataList = "";
+
+            while (resultSet.next()) {
+
+                ReceptKompDTO receptKomp = getReceptKomp(resultSet);
+
+               receptKompList.add(receptKomp);
+            }
+            connection.close();
+
+            return receptKompList;
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("findes ikke");
+        }
+
+
         return null;
     }
 
+
     @Override
     public void createReceptKomp(ReceptKompDTO receptkomponent) throws DALException {
+        try {
+            Class.forName(this.driver);
+            String sqlManipulation = "INSERT ReceptKomp VALUES ('" + receptkomponent.getReceptId() + "', '" + receptkomponent.getRaavareId() + "', '" + receptkomponent.getNomNetto() + "', '" + receptkomponent.getTolerance() + "')";
+
+            Connection connection = DriverManager.getConnection(this.url, this.username, this.password);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sqlManipulation);
+            connection.close();
+
+        } catch (SQLException | ClassNotFoundException e ) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void updateReceptKomp(ReceptKompDTO receptkomponent) throws DALException {
+        try {
+            Class.forName(this.driver);
+            String sqlManipulation = "UPDATE ReceptKomp SET receptId = " + receptkomponent.getReceptId() + ", raavareId = '" + receptkomponent.getRaavareId() + "', nomNetto = '" + receptkomponent.getNomNetto() + "', tolerance = '" + receptkomponent.getTolerance() +"' WHERE receptId = " + receptkomponent.getReceptId();
+            Connection connection = DriverManager.getConnection(this.url, this.username, this.password);
+
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sqlManipulation);
+            connection.close();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     }
-}
+
