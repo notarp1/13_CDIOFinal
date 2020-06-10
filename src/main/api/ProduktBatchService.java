3cc50cbae2c9291112bff3b226d690cbebf8e57e
christian <ChristiaNotarmaso@gmail.com>
2020-06-10 15:37:03 +0200

    API ProduktBatchServicetch

--- src/main/api/ProduktBatchService.java
+++ src/main/api/ProduktBatchService.java
@@ -1,16 +1,37 @@
 package api;
 
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import DTO.ProduktBatchDTO;
+import controller.Classes.ProduktBatchController;
+import controller.ControllerException;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("pbService")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ProduktBatchService {
 
+    private ProduktBatchController pController = new ProduktBatchController();
+
+
+    @Path("getPB")
+    @GET
+    public Response getProduktBatch(@QueryParam("pbId") int pbId){
+
+        try {
+
+            return Response.status(Response.Status.OK).entity(pController.getProduktBatch(pbId)).build();
+
+        } catch (ControllerException e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+
+
+    }
 
 
 
