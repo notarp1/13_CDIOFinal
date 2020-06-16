644c33ce21dbcbd0f5feacdd75cf7eb6d815830c
Folkmann <theme1256@gmail.com>
2020-06-16 11:27:11 +0200

    Fix af menu, så den ser korrekt ud uanset hvor mange punkter der vises

--- src/test/java/controller/Classes/RaavareBatchControllerTest.java
+++ src/test/java/controller/Classes/RaavareBatchControllerTest.java
@@ -2,15 +2,7 @@ package controller.Classes;
 
 import DTO.RaavareBatchDTO;
 import controller.ControllerException;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 
 public class RaavareBatchControllerTest {
