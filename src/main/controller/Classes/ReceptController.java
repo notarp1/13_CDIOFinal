b0f69136e6432419fd2ae1f5571cf5f0e94bd5ec
Ismail <s195460@student.dtu.dk>
2020-06-17 13:59:17 +0200

    receptController limits og små ændringer

--- src/main/controller/Classes/ReceptController.java
+++ src/main/controller/Classes/ReceptController.java
@@ -23,9 +23,7 @@ public class ReceptController implements IReceptController {
     @Override
     public ReceptDTO getRecept(int receptId) throws ControllerException {
 
-        if(receptId > 99999999 || receptId < 1) {
-            throw new ControllerException("Ugyldig ID");
-        }
+        rangeLimitR(receptId);
         try {
                 return recpetData.getRecept(receptId);
 
@@ -47,6 +45,8 @@ public class ReceptController implements IReceptController {
 
     @Override
     public void createRecept(ReceptDTO recept) throws ControllerException {
+        rangeLimitR(recept.getReceptId());
+        RNavnLimit(recept.getReceptNavn());
         try {
             recpetData.createRecept(recept);
         } catch (DALException e) {
@@ -58,6 +58,8 @@ public class ReceptController implements IReceptController {
 
     @Override
     public void updateRecept(ReceptDTO recept) throws ControllerException {
+        rangeLimitR(recept.getReceptId());
+        RNavnLimit(recept.getReceptNavn());
         try {
             recpetData.updateRecept(recept);
         } catch (DALException e) {
@@ -80,13 +82,7 @@ public class ReceptController implements IReceptController {
 
     @Override
     public ReceptKompDTO getReceptKomp(int receptId, int raavareId) throws ControllerException {
-        if(receptId < 1 || receptId > 99999999 ){
-            throw new ControllerException("Ugyldig receptId");
-        }
-        if(raavareId < 1 || raavareId > 99999999 ){
-            throw new ControllerException("Ugyldig råvareID");
-        }
-
+        rangeLimitRKomp1(receptId, raavareId);
         try {
             return  receptKompD.getReceptKomp(receptId,raavareId);
         } catch (DALException e) {
@@ -98,8 +94,7 @@ public class ReceptController implements IReceptController {
 
     @Override
     public List<ReceptKompDTO> getReceptKompList(int receptId) throws ControllerException {
-        if(receptId < 1 || receptId > 99999999 )
-            throw new ControllerException("Ugyldig ID");
+        rangeLimitR(receptId);
 
         try {
             return receptKompD.getReceptKompList(receptId);
@@ -112,6 +107,7 @@ public class ReceptController implements IReceptController {
 
     @Override
     public List<ReceptKompDTO> getReceptKompList() throws ControllerException {
+
         try {
             return receptKompD.getReceptKompList();
         } catch (DALException e) {
@@ -123,6 +119,7 @@ public class ReceptController implements IReceptController {
 
     @Override
     public void createReceptKomp(ReceptKompDTO receptkomponent) throws ControllerException {
+        rangeLimitRKomp2(receptkomponent.getReceptId(), receptkomponent.getRaavareId(), receptkomponent.getNomNetto(), receptkomponent.getTolerance());
         try {
             receptKompD.createReceptKomp(receptkomponent);
         } catch (DALException e) {
@@ -134,6 +131,7 @@ public class ReceptController implements IReceptController {
 
     @Override
     public void updateReceptKomp(ReceptKompDTO receptkomponent) throws ControllerException {
+        rangeLimitRKomp2(receptkomponent.getReceptId(), receptkomponent.getRaavareId(), receptkomponent.getNomNetto(), receptkomponent.getTolerance());
         try {
             receptKompD.updateReceptKomp(receptkomponent);
         } catch (DALException e) {
@@ -154,4 +152,41 @@ public class ReceptController implements IReceptController {
 
     }
 
+
+
+
+    private void rangeLimitR(int receptId) throws ControllerException {
+        if(receptId < 1 || receptId > 99999999 )
+            throw new ControllerException("uden for range 1-99999999");
+    }
+
+    private void RNavnLimit(String receptNavn) throws ControllerException {
+        if(receptNavn.length() < 2 || receptNavn.length() > 20 )
+            throw new ControllerException("Recept Navn limit er 2-20");
+    }
+
+
+
+    private void rangeLimitRKomp1(int receptId, int raavareId) throws ControllerException {
+        if (receptId < 1 || receptId > 99999999)
+            throw new ControllerException("uden for receptID range 1-99999999");
+        if (raavareId < 1 || raavareId > 99999999) {
+            throw new ControllerException("Uden for raavareID range 1-99999999");
+        }
+
+    }
+
+    private void rangeLimitRKomp2(int receptId, int raavareId, double nomNetto, double tolerance) throws ControllerException {
+        if(receptId < 1 || receptId > 99999999 )
+            throw new ControllerException("uden for receptID range 1-99999999");
+        if(raavareId < 1 || raavareId > 99999999 ){
+            throw new ControllerException("Uden for raavareID range 1-99999999");
+        }
+        if(nomNetto < 0.05 || nomNetto > 20.0 ){
+            throw new ControllerException("Uden for raavareID range 1-99999999");
+        }
+        if(tolerance < 0.1 || tolerance > 10 ){
+            throw new ControllerException("Uden for raavareID range 1-99999999");
+        }
+    }
 }
