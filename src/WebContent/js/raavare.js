5b646134f617db88d787f74aaf40f002eb5ad882
Christine <s141361@student.dtu.dk>
2020-06-17 12:16:22 +0200

    tilføjet mere til raavarejs

--- src/WebContent/js/raavare.js
+++ src/WebContent/js/raavare.js
@@ -41,7 +41,32 @@ $("updateRaa").submit(function (event){
     });
 });
 
+function raavareList() {
+    var getRaaList = $("#getRaaList");
+    $.ajax({
+        type: "GET",
+        url: "api/raaService/getRaaList",
+        contentType: "application/JSON",
+        success: function (data) {
+            console.log(data);
+            data.forEach(function (data) {
+                console.log(data);
+                getRaaList.append(`<tr>
+                    <td>${data.raavareId}</td>
+                    <td>${data.raavareNavn}</td>
+                    <td>${data.leverandoer}</td>
+                    <td><button onclick="deleteRecept(${data.raavareId})">slet</button></td>
+                </tr>`);
+            })
 
+        },
+        error: function (XHR) {
+            console.log(XHR);
+            alert("Fejl: " + XHR.responseText);
+        }
+    })
+
+}
 
 
 
